0b680b217f8fb30a7546261cdc78995192f4ef8d
Rasmus Glasell Johansson <ragl025@gmail.com>
2023-09-28 11:02:39 +0200

    Added menu

--- Program.cs
+++ Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Meny();
         }
+        static void Meny()
+        {
+            bool program = true;
+
+            while (program)
+            {
+                Console.Clear();
+                Console.WriteLine("Välkommen till kalkylatorn");
+                Console.WriteLine("[1] Addition");
+                Console.WriteLine("[2] Subtraktion");
+                Console.WriteLine("[3] Multiplikation");
+                Console.WriteLine("[4] Division");
+                Console.WriteLine("[5] Avsluta programmet");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        Console.WriteLine("Programmet avslutas. . .");
+                        program = false;
+                        break;
+                }
+            }
+        }
+
+        static int Multiplikation()
+        {
+            Console.Write("Skriv in ditt första tal du vill multiplicera: ");
+
+        }
+
     }
 }
