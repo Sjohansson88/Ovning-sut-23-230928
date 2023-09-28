9eed5df431863f44c3f72666337f511507e2db6e
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-09-28 11:38:17 +0200

    Addition löst

--- Program.cs
+++ Program.cs
@@ -12,15 +12,14 @@ namespace Ovning_sut_23_230928
         public void Addition()
         {
             int[] arr = new int[2];
-            arr[0] = Convert.ToInt32(Console.ReadLine());
-            arr[1] = Convert.ToInt32((Console.ReadLine()));
 
             Console.WriteLine("Skriv in det första tal du vill addera:");
-            Console.ReadLine();
+            arr[0] = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Skriv in det andra tal du vill addera:");
-            Console.ReadLine();
+            arr[1] = Convert.ToInt32((Console.ReadLine()));
 
-            Console.WriteLine("{0} adderat med {0} = {0}.", arr[0], arr[1], (arr[0] + arr[1]));
+            Console.WriteLine("{0} adderat med {1} = {2}.", arr[0], arr[1], (arr[0] + arr[1]));
+            Console.ReadKey();
 
 
             
@@ -28,6 +27,7 @@ namespace Ovning_sut_23_230928
         }
         static void Meny()
         {
+            Program add = new Program();
             bool program = true;
 
             while (program)
@@ -44,6 +44,7 @@ namespace Ovning_sut_23_230928
                 switch (choice)
                 {
                     case 1:
+                        add.Addition();
                         break;
                     case 2:
                         break;
@@ -59,11 +60,11 @@ namespace Ovning_sut_23_230928
             }
         }
 
-        static int Multiplikation()
-        {
-            Console.Write("Skriv in ditt första tal du vill multiplicera: ");
+        //static int Multiplikation()
+        //{
+        //    Console.Write("Skriv in ditt första tal du vill multiplicera: ");
 
-        }
+        //}
 
     }
 }
