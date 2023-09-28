fd9e2479a9b643ca35f2b39801fea3a126f79a54
Stefan J <sjohansson88@hotmail.com>
2023-09-28 10:41:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ovning_sut_23_230928
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
